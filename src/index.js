1502f3cc25a66471346aac6fa97d53065033a750
Ak6706072 <42826806+Ak6706072@users.noreply.github.com>
2021-05-10 02:53:19 +0530

    initial changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+const input = document.getElementById("input");
+
+const getdata = (url, value) => {
+  console.log(url, value, input.value);
+};
+
+const throttle = (func, limit) => {
+  let flag = true;
+  return () => {
+    if (flag) {
+      func();
+      flag = false;
+      setTimeout(() => {
+        flag = true;
+      }, limit);
+    }
+  };
+};
+const betterFxn = throttle(getdata, 200);
+
+input.addEventListener("keypress", betterFxn);
